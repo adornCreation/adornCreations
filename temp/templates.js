1dd16735bf7d49b3fa8b27bdb1c825360f61b499
Rahul Singh <rahulsb210@gmail.com>
2015-05-28 22:22:26 +0530

    third commit

---
+++ temp/templates.js
@@ -0,0 +1,8 @@
+angular.module('uiAdornCreation').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('src/client/adornCreations/core/menubar.html',
+    "<div class=\"menu001 Jumbotron\"><div class=menu00101>first one menu</div><div class=menu00102><div class=firstDivOfMenu>hiit tghere</div><div><input class=\"form-control input-md\" type=text id=searchbox placeholder=\"search item, product or events\"></div><div><button class=\"btn btn-info searchBtn\">Search</button></div></div></div><div class=menuTail01></div>"
+  );
+
+}]);
